3a2dbfb54eba9ffbb0a39e3c722feee421d30df5
NathanPerezR <nathanperez.business@gmail.com>
2024-07-21 08:28:04 -0500

    added hex

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -74,7 +74,34 @@ local plugins = {
         require('neoscroll').setup {}
       end,
     lazy = false,
-  }
+  },
+  {
+    "RaafatTurki/hex.nvim",
+    config = function()
+      require("hex").setup {
+        -- cli command used to dump hex data
+        dump_cmd = 'xxd -g 1 -u',
+        
+        -- cli command used to assemble from hex data
+        assemble_cmd = 'xxd -r',
+        
+        -- function that runs on BufReadPre to determine if it's binary or not
+        is_buf_binary_pre_read = function()
+          -- logic that determines if a buffer contains binary data or not
+          -- must return a bool
+          return false -- Replace with actual logic
+        end,
+        
+        -- function that runs on BufReadPost to determine if it's binary or not
+        is_buf_binary_post_read = function()
+          -- logic that determines if a buffer contains binary data or not
+          -- must return a bool
+          return false -- Replace with actual logic
+        end,
+      }
+    end,
+    lazy = false,
+  },
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
